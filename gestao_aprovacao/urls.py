30e9d679e015f181a9d48540d94b621e515cb831
Felipe Bandeira <felipebqueiroz@hotmail.com>
2026-04-01 19:00:19 -0300

    Revert "feat: botão snapshot PDF na listagem do GestControll"
    This reverts commit bfe2664250c5521aa5d1e09aae65368811b4c06c.

--- gestao_aprovacao/urls.py
+++ gestao_aprovacao/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path('pedidos/', views.list_workorders, name='list_workorders'),
     path('pedidos/criar/', views.create_workorder, name='create_workorder'),
     path('pedidos/<int:pk>/', views.detail_workorder, name='detail_workorder'),
-    path('pedidos/<int:pk>/exportar-pdf/', views.exportar_snapshot_workorder_pdf, name='exportar_snapshot_workorder_pdf'),
     path('pedidos/<int:pk>/editar/', views.edit_workorder, name='edit_workorder'),
     
     # Aprovação
